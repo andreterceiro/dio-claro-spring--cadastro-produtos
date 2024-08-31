b92f595206335f45254920486330c6ba5f21d0b0
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-08-30 23:42:14 -0300

    In the class CadastroProdutos we implemented a static method to help us in the task of manual tests

--- src/CadastroProdutos.java
+++ src/CadastroProdutos.java
@@ -49,4 +49,21 @@ public class CadastroProdutos {
         produtosPorPreco.addAll(this.produtoSet);
         return produtosPorPreco;
     }
+
+    /**
+     * Main method that helps us in manual tests
+     *
+     * @param args Arguments passed through CLI (ignored)
+     */
+    public static void main(String[] args) {
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+        System.out.println(cadastroProdutos.exibirProdutosPorNome());
+        // See that will be not allowed 2 products with the same code (different in other properties)
+        cadastroProdutos.adicionarProduto(1L, "Lápis", 1.5D, 3);
+        cadastroProdutos.adicionarProduto(1L, "Lápis", 2.5D, 2);
+        cadastroProdutos.adicionarProduto(3L, "Borracha", 2D, 5);
+        cadastroProdutos.adicionarProduto(4L, "Apontador", 3.2D, 1);
+        System.out.println(cadastroProdutos.exibirProdutosPorNome());
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+    }
 }
